4afa633750c865c584af46faedfd8887d2981607
katieb786 <katiebond33091@gmail.com>
2023-01-17 15:43:13 -0500

    Day 1

---
+++ Day 1/main.py
@@ -0,0 +1,55 @@
+print("Hello world!")
+
+# print() will print whatever you put in the parentheses in the consol.
+# Strings are in "", '' works as well
+# Strings are not code just text
+# If you forget the "" you will get a syntax error.
+# Stackoverflow.com is a great way to figure out what your errors mean.
+# If you've already used "" you must use '' if it's on the same line.
+    # eg. print("print('what to print')")
+    # will cause an error if you use "" for both
+
+print("Hello world!\nHello world!")
+
+# \n will allow you to print on next line using the same print function
+
+print("Hello" + " " + "Angela")
+
+# This will print Hello Angela
+# using + is called string concaatenation
+# This merges two strings together
+# You must put " " if you want spaces between the strings or add a space before or after
+# Spaces are very important in python
+    # Indentation errors will occur if you misplace spaces at the start of a line
+
+print("Hello " + input("What is your name? "))
+
+# Input funciton will let the user input something
+# It will pause the program until the user inputs something
+
+print(len(input("What is your name? ")))
+
+# len() will calculate the length or how many characters in a string
+
+name = "Jack"
+print(name)
+
+# variables will allow you to name data and save it.
+
+name = "Angela"
+print(name)
+
+# you change variables:
+    #this will print the more updated version of name
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+# Make sure your names make sense and will be able to be clear if you come back later.
+# When using variables later make sure you spell things correctly and consistantly throughout your code
+# You will get a name error if your variable doesn't match something you named before.
+
+
+
+
