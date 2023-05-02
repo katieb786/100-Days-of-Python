fcfe60c1af1a7d9400740a8b9dbd5220a2b7d7cd
katiebond <katiebond33091@gmail.com>
2023-05-01 20:48:41 -0400

    day 3 part 2

---
+++ pythonProject/day 3 part 2/main.py
@@ -0,0 +1,36 @@
+# print("Welcome to Python Pizza Deliveries!")
+# size = input("What size pizza do you want? S, M, or L ")
+# add_pepperoni = input("Do you want pepperoni? Y or N ")
+# extra_cheese = input("Do you want extra cheese? Y or N ")
+#
+# total = 0
+#
+# if size == "S":
+#     total += 15
+#     if add_pepperoni == "Y":
+#         total += 2
+#     if extra_cheese == "Y":
+#         total += 1
+# elif size == "M":
+#     total += 20
+#     if add_pepperoni == "Y":
+#         total += 3
+#     if extra_cheese == "Y":
+#         total += 1
+# else:
+#     total += 25
+#     if add_pepperoni == "Y":
+#         total += 3
+#     if extra_cheese == "Y":
+#         total += 1
+#
+# print(f"Your final bill is: ${total}.")
+
+#-----------------------------------------------------
+
+# Logical Operators
+    # combining coditionals
+    # A and B, A or B, not A
+
+
+
