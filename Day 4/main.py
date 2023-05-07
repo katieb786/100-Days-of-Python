0a86afcadb59b33589e0ae8b70d9312cd0e70ea3
katiebond <katiebond33091@gmail.com>
2023-05-07 17:46:38 -0400

    Day 4 end

--- Day 4/main.py
+++ Day 4/main.py
@@ -34,11 +34,15 @@
 # # Store group pieces of data
 # # list = [item1, item2]
 states_of_america = ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland',
-'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky',
-'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas',
-'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia',
-'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah',
-'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
+                     'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island',
+                     'Vermont', 'Kentucky',
+                     'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine',
+                     'Missouri', 'Arkansas',
+                     'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas',
+                     'West Virginia',
+                     'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho',
+                     'Wyoming', 'Utah',
+                     'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
 # # The order you put items in is saved
 # print(states_of_america[0])
 # # Delaware
@@ -68,3 +72,37 @@ states_of_america = ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Conne
 # print(f"{payer} is going to buy the meal today!")
 
 # ------------------------------------------------------------------------------------
+
+# Index out of range error: Trying to get a position out of the total number of items.
+
+# print(states_of_america[50]
+num_of_states = len(states_of_america)
+# print(states_of_america[num_of_states])
+
+print(states_of_america[num_of_states - 1])
+
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches",
+#                "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+# Nested lists:
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
+
+# ------------------------------------------------------------------------------------------------------------
+
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+column = int(position[0]) - 1
+row = int(position[1]) - 1
+
+map[row][column] = "X"
+print(f"{row1}\n{row2}\n{row3}")
